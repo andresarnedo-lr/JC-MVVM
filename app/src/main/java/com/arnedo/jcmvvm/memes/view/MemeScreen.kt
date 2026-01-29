b2395c7cfb7573c1333c6da9b7c28cc9a777dcaa
Andres Arnedo <alfredoandresarnedo@gmail.com>
2026-01-29 17:24:54 +0100

    feature(view): Añadimos la vista de cada ItemMeme y primeros arreglos en la view principal

--- app/src/main/java/com/arnedo/jcmvvm/memes/view/MemeScreen.kt
+++ app/src/main/java/com/arnedo/jcmvvm/memes/view/MemeScreen.kt
@@ -3,18 +3,15 @@ package com.arnedo.jcmvvm.memes.view
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.arnedo.jcmvvm.R
 import com.arnedo.jcmvvm.ui.components.MyProgressFullScreen
 import com.arnedo.jcmvvm.ui.components.MyTextTitle
-
-@Preview(showBackground = true)
-@Composable
-fun MainViewPreview() {
-    MainView(Modifier)
-}
 
 
 @Composable
@@ -22,8 +19,22 @@ fun MainView(modifier : Modifier) {
     Box(modifier){
         Column(){
             MyTextTitle(R.string.app_name)
+
+            LazyVerticalGrid(columns = GridCells.Adaptive(
+                minSize = dimensionResource(R.dimen.img_grid_size)
+            )){
+            }
         }
 
         AnimatedVisibility(visible = false) { MyProgressFullScreen() }
     }
+}
+
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun MainViewPreview() {
+    MainView(Modifier)
 }
